4ff1d6b5dc09540006d012d0596486a840889d62
breandan <bre@ndan.co>
2020-06-18 18:08:58 -0400

    refactor

--- pantograph.py
+++ pantograph.py
@@ -3,22 +3,23 @@ from PIL import Image
 from io import BytesIO
 
 
-class Capture(ast.NodeVisitor):
+class PCapture(ast.NodeVisitor):
     def __init__(self, parsed, par_names):
-        self.duc = beniget.DefUseChains()
-        self.duc.visit(parsed)
-        self.udc = beniget.UseDefChains(self.duc)
-        self.params = {k: v for (k, v) in self.udc.chains.items() if hasattr(k, 'id') and k.id in par_names and v}
+        self.def_use = beniget.DefUseChains()
+        self.def_use.visit(parsed)
+        self.use_def = beniget.UseDefChains(self.def_use)
+        self.params = {k: v for (k, v) in self.use_def.chains.items() if hasattr(k, 'id') and k.id in par_names and v}
         self.graph = pydot.Dot(graph_type='digraph')
         self.node_to_dot_node = {}
         self.src_to_target = set()
-        self.visited = set()
+        self.already_visited = set()
         for param_node in self.params.keys():
             self.visit(param_node)
 
-    def add_edge_if_nonexistient(self, source, target):
+    def add_edge_if_nonexistent(self, source, target):
         if (source, target) not in self.src_to_target:
             self.graph.add_edge(pydot.Edge(source, target))
+            self.src_to_target.add((source, target))
 
     def get_or_create_dot_node(self, node):
         if node in self.node_to_dot_node:
@@ -30,18 +31,18 @@ class Capture(ast.NodeVisitor):
             return new_node
 
     def visit(self, node):
-        if node in self.visited:
+        if node in self.already_visited:
             return
 
-        pdsource = self.get_or_create_dot_node(node)
-        self.visited.add(node)
-        users = self.udc.chains[node][0].users()
+        dot_source = self.get_or_create_dot_node(node)
+        self.already_visited.add(node)
+        users = self.use_def.chains[node][0].users()
         if not users:
             return
 
-        pdtargets = [self.get_or_create_dot_node(x) for x in users]
-        for pdtarget in pdtargets:
-            self.add_edge_if_nonexistient(pdsource, pdtarget)
+        dot_targets = [self.get_or_create_dot_node(x) for x in users]
+        for dot_target in dot_targets:
+            self.add_edge_if_nonexistent(dot_source, dot_target)
 
         for user in users:
             self.visit(user.node)
@@ -50,22 +51,24 @@ class Capture(ast.NodeVisitor):
 class PGraph:
     def __init__(self, *par_values):
         # Check Jupyter notebook input cache
-        jupyter_cache = (stack.frame.f_locals['_ih'] for stack in inspect.stack() if '_ih' in stack.frame.f_locals)
-        self.src = next(jupyter_cache, None)
-        if self.src is not None:
-            self.src = '\n'.join(self.src)
-
-        # Check REPL history
-        if self.src is None:
-            self.src = [str(readline.get_history_item(i + 1)) for i in range(readline.get_current_history_length())]
-        # Check Python file
-        if self.src is None or not self.src:
-            self.src = self.__get_caller_src()
-
+        self.src = self.recover_source()
         par_names = self.__get_caller_params()
         parsed = ast.parse(self.src)
-        capture = Capture(parsed, par_names)
+        capture = PCapture(parsed, par_names)
         self.graph = capture.graph
+
+    def recover_source(self):
+        jupyter_cache = (stack.frame.f_locals['_ih'] for stack in inspect.stack() if '_ih' in stack.frame.f_locals)
+        src = next(jupyter_cache, None)
+        if src is not None:
+            src = '\n'.join(src)
+        # Check REPL history
+        if src is None:
+            src = [str(readline.get_history_item(i + 1)) for i in range(readline.get_current_history_length())]
+        # Check Python file
+        if src is None or not src:
+            src = self.__get_caller_src()
+        return src
 
     def __get_caller_src(self):
         frames = inspect.getouterframes(inspect.currentframe())
